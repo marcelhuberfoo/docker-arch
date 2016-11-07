833d13d4edfac8d9c2dfcd8519daa5a2950f4282
Marcel Huber <marcelhuberfoo@gmail.com>
2016-11-07 11:16:18 +0100

    push latest and tagged image only

--- docker_push.sh
+++ docker_push.sh
@@ -3,7 +3,7 @@
 set -e
 . ./build_params.sh
 
-docker push $docker_image
-docker tag $docker_image $docker_image:$(docker run -it --rm --entrypoint /bin/sh $docker_image -c 'echo -n ${BUILD_DATE}_${KERNEL_VERSION}')
+docker push ${docker_image}:latest
+docker tag ${docker_image}:latest $docker_image:$(docker run -it --rm --entrypoint /bin/sh $docker_image -c 'echo -n ${BUILD_DATE}_${KERNEL_VERSION}')
 docker push $docker_image:$(docker run -it --rm --entrypoint /bin/sh $docker_image -c 'echo -n ${BUILD_DATE}_${KERNEL_VERSION}')
 
